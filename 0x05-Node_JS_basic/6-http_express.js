aad0d964c77457065fe8a23378b663c0ea9ee053
Joshuawilfred <joewilfred216@gmail.com>
2025-01-26 12:26:20 +0300

    Add new project files

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,12 +1,11 @@
-const express = require('express');
+const express = require("express");
 
-const app = express();
 const port = 1245;
 
-app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.send('Hello Holberton School!');
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Holberton School!");
 });
 
 app.listen(port);
