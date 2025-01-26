1c12d6a9d2e1e5a67850bc5907abe81dec8c6b9e
Joshuawilfred <joewilfred216@gmail.com>
2025-01-26 12:26:20 +0300

    Add new project files

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,30 +1,44 @@
-const fs = require('fs');
+const fs = require("fs");
 
-function countStudents(path) {
-  if (!fs.existsSync(path)) {
-    throw Error('Cannot load the database');
+const getDataFromCsvFile = (path) => {
+  const csvData = fs.readFileSync(path, "utf8");
+  const rows = csvData.trim().split("\n");
+  return rows.slice(1).map((row) => row.split(","));
+};
+
+const assignNamesWithFields = (results) => {
+  const fieldsWithFirstName = {};
+
+  results.forEach((entry) => {
+    const field = entry[3];
+    const firstName = entry[0];
+
+    if (!fieldsWithFirstName[field]) {
+      fieldsWithFirstName[field] = [];
+    }
+    fieldsWithFirstName[field].push(firstName);
+  });
+
+  return fieldsWithFirstName;
+};
+
+const printFieldStats = (fieldWithName) => {
+  for (const [key, value] of Object.entries(fieldWithName)) {
+    const list = value.join(", ");
+    console.log(`Number of students in ${key}: ${value.length}. List: ${list}`);
   }
-  // block other parallel process
-  // and do the current file reading process
-  const data = fs.readFileSync(path, 'utf8');
-  const students = data.split('\n')
-    .map((student) => student.split(','))
-    .filter((student) => student.length === 4 && student[0] !== 'firstname')
-    .map((student) => ({
-      firstName: student[0],
-      lastName: student[1],
-      age: student[2],
-      field: student[3],
-    }));
-  const csStudents = students
-    .filter((student) => student.field === 'CS')
-    .map((student) => student.firstName);
-  const sweStudents = students
-    .filter((student) => student.field === 'SWE')
-    .map((student) => student.firstName);
-  console.log(`Number of students: ${students.length}`);
-  console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
-  console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-}
+};
+
+const countStudents = (path) => {
+  try {
+    const results = getDataFromCsvFile(path);
+    const fieldsWithFirstName = assignNamesWithFields(results);
+
+    console.log(`Number of students: ${results.length}`);
+    printFieldStats(fieldsWithFirstName);
+  } catch (error) {
+    throw new Error("Cannot load the database");
+  }
+};
 
 module.exports = countStudents;
